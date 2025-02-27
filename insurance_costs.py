f97c84eab64df4eabb238d3bdf9a03af0fa463ac
matt-bain <bainm97@gmail.com>
2025-02-27 13:06:08 +0000

    Calculate average cost for smoker vs non-smoker

--- insurance_costs.py
+++ insurance_costs.py
@@ -30,4 +30,13 @@ average_female_cost = female_total_cost / female_total_count
 print(f"The average cost of insurance for males is: ${average_male_cost:.2f} and for females is: ${average_female_cost:.2f}")
 print(f"{(male_total_count/len(insurance_records)*100):.2f}% of records are male and {(female_total_count/len(insurance_records)*100):.2f}% are female")
 
+#This was an unexpected result as generally female insurance is more expensive
+#2. Getting average cost for a smoker vs non-smoker
 
+smoker_total_cost, smoker_total_count, non_smoker_total_cost, non_smoker_total_count = get_total_and_count(insurance_records, 'smoker', 'yes', 'no')
+
+average_smoker_cost = smoker_total_cost / smoker_total_count
+average_non_smoker_cost = non_smoker_total_cost / non_smoker_total_count
+
+print(f"The average cost of insurance for smokers is: ${average_smoker_cost:.2f} and for non_smokers is: ${average_non_smoker_cost:.2f}")
+print(f"{(smoker_total_count/len(insurance_records)*100):.2f}% of records are smokers and {(non_smoker_total_count/len(insurance_records)*100):.2f}% are non-smokers")
