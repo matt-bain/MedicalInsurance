9e2f88127ebad7d787e137b81577bce18ca4fce2
matt-bain <bainm97@gmail.com>
2025-02-27 13:10:58 +0000

    Named the calculation function more sensibly

--- insurance_costs.py
+++ insurance_costs.py
@@ -7,7 +7,7 @@ with open('insurance.csv', newline='') as file:
     for row in reader:
         insurance_records.append(row)
 
-def get_total_and_count(records, comp_record, comp_crit_1, comp_crit_2):
+def get_cost_and_count(records, comp_record, comp_crit_1, comp_crit_2):
     crit_1_total_cost, crit_1_total_count = 0, 0
     crit_2_total_cost, crit_2_total_count = 0, 0
 
@@ -22,7 +22,7 @@ def get_total_and_count(records, comp_record, comp_crit_1, comp_crit_2):
     return crit_1_total_cost, crit_1_total_count, crit_2_total_cost, crit_2_total_count
 #1. find average cost of insurance for male vs female
 #start with finding totals for each sex and the count
-male_total_cost, male_total_count, female_total_cost, female_total_count = get_total_and_count(insurance_records, 'sex', 'male', 'female')
+male_total_cost, male_total_count, female_total_cost, female_total_count = get_cost_and_count(insurance_records, 'sex', 'male', 'female')
 
 average_male_cost = male_total_cost / male_total_count
 average_female_cost = female_total_cost / female_total_count
@@ -33,7 +33,7 @@ print(f"{(male_total_count/len(insurance_records)*100):.2f}% of records are male
 #This was an unexpected result as generally female insurance is more expensive
 #2. Getting average cost for a smoker vs non-smoker
 
-smoker_total_cost, smoker_total_count, non_smoker_total_cost, non_smoker_total_count = get_total_and_count(insurance_records, 'smoker', 'yes', 'no')
+smoker_total_cost, smoker_total_count, non_smoker_total_cost, non_smoker_total_count = get_cost_and_count(insurance_records, 'smoker', 'yes', 'no')
 
 average_smoker_cost = smoker_total_cost / smoker_total_count
 average_non_smoker_cost = non_smoker_total_cost / non_smoker_total_count
